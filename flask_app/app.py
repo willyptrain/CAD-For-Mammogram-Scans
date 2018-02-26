6eb2c5c19880deb9e48495380739c9cfe44e0900
Will Peterson <willpeterson76@gmail.com>
2018-02-25 22:12:24 -0500

    sqlite3 working

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,68 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, request
+import sqlite3
+
 app = Flask(__name__)
 
-@app.route("/")
 
-def main():
-    return render_template('index.html')
+DATABASE = 'database.db'
+
+db = sqlite3.connect(DATABASE)
+print("opened db")
+db.execute('create table if not exists patients (case_num TEXT, name TEXT, age TEXT, assessment TEXT)')
+print("added table")
+db.close()
+
+
+
+@app.route("/")
+def home():
+   db = sqlite3.connect(DATABASE)
+   db.row_factory = sqlite3.Row
+   cur = db.cursor()
+   cur.execute("select * from patients")
+   rows = cur.fetchall()
+   
+   return render_template('index.html', rows = rows)
+    
+@app.route('/new_pat')
+def new_patient():
+   return render_template('new_patient.html')
+   
+   
+@app.route('/addpat',methods = ['POST', 'GET'])
+def addpat(): 
+   msg = ""
+   if request.method == 'POST':
+      try:
+         case_num = request.form['case']
+         name = request.form['name']
+         age = request.form['age']
+         assessment = request.form['assessment']
+         
+         with sqlite3.connect(DATABASE) as db:
+            cur = db.cursor()
+            cur.execute("INSERT INTO patients (case_num, name, age, assessment)  VALUES (?,?,?,?)",(case_num,name,age,assessment) )
+            print("2")  
+            db.commit()
+            print("3")
+            msg = "Record successfully added"
+      except:
+         db.rollback()
+         msg = "error in insert operation"
+         
+      finally:      
+         return render_template("status_update.html",msg = msg)
+         db.close()
+   
+
+   
+
+    
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+    
+    
+    
+    
+    
